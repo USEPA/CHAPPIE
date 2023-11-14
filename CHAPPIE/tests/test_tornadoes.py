fb5ee2fb56cd2c98c4301769c52777a180b5f547
Bousquin <Bousquin.Justin@epa.gov>
2023-11-14 17:02:56 -0600

    Added expected file names

--- CHAPPIE/tests/test_tornadoes.py
+++ CHAPPIE/tests/test_tornadoes.py
@@ -21,7 +21,8 @@ AOI = os.path.join(DATA_DIR, "BreakfastPoint_ServiceArea.shp")
 
 def test_get_tornadoes():
     actual = tornadoes.get_tornadoes()
-    expected_file = os.path.join(EXPECTED_DIR, '<FILE>')
+    #NOTE/TODO: 1950-2022-torn-aspath is too big to save in expected?
+    expected_file = os.path.join(EXPECTED_DIR, '1950-2022-torn-aspath')
     expected = geopandas.read_file(expected_file)
     
     assert_geodataframe_equal(actual, expected, check_like=True)
@@ -30,7 +31,7 @@ def test_get_tornadoes():
 def test_process_tornadoes():
     #aoi_input = geopandas.read_file()  # may be able to pass it filename
     actual = tornadoes.process_tornadoes()
-    expected_file = os.path.join(EXPECTED_DIR, '<FILE>')
+    expected_file = os.path.join(EXPECTED_DIR, 'FLROAR20231108_Torn_Buffer_AOI_Intersection_1996_2016.shp')
     expected = geopandas.read_file(expected_file)
     
     assert_geodataframe_equal(actual, expected, check_like=True)
