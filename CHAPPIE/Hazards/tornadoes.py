fcee5887c306f0a04114207dfdab51e968126c0a
Bousquin <Bousquin.Justin@epa.gov>
2023-11-14 17:06:29 -0600

    Outlined two functions for submodule

---
+++ CHAPPIE/Hazards/tornadoes.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Test tropical cyclones 
+
+@author: jbousqui
+"""
+
+def get_tornadoes():
+    
+    return gdf
+
+def process_tornadoes(tornadoe_paths, aoi):
+    
+    return gdf
