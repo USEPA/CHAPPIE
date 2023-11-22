d624b0dd585e25ae2f867177f130b1541875c793
Bousquin <Bousquin.Justin@epa.gov>
2023-11-22 09:15:59 -0600

    Initial automated conversion of process steps 1 & 2

--- CHAPPIE/Hazards/tornadoes.py
+++ CHAPPIE/Hazards/tornadoes.py
@@ -25,6 +25,25 @@ def get_tornadoes(out_dir):
 
 
 
-def process_tornadoes(tornadoe_paths, aoi):
+
+def process_tornadoes(tornadoe_dir, aoi):
     
     return gdf
+
+#import magrittr
+#import sf
+#import purrr
+#import dplyr
+
+torn_path = geopandas.read_file("L:\\Priv\\SHC_1012\\Florida ROAR\\Data\\Hazards\\Tornadoes\\1950-2022-torn-aspath\\1950-2022-torn-aspath", "1950-2022-torn-aspath")
+
+def project_torn_path(paths, zone):
+    if len(zone) > 1:
+        return purrr.map(.x = unlist(lapply(zone, lambda x: paste0("+proj=utm +zone=", x, " +ellps=GRS80 +units=m +no_defs"))),
+                         .f = sf.st_transform,
+                         x = paths) %>%
+               purrr.set_names(unlist(lapply(zone, lambda x: paste0("torn_proj_", x)))) %>%
+               lapply(., lambda x: {cbind(x, radM = (x.wid / 2) / 1.094)})
+    else:
+        return sf.st_transform(paths, paste0("+proj=utm +zone=", zone, " +ellps=GRS80 +units=m +no_defs")) |>
+               dplyr.mutate(radM = (wid / 2) / 1.094)
