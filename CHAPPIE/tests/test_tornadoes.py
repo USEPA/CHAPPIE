6a313ecee27eddd25f4b2ab60024ab49d2173287
Bousquin <Bousquin.Justin@epa.gov>
2024-03-19 17:08:53 -0500

    Sort on number not wind (wid). check_less_precise just in case.

--- CHAPPIE/tests/test_tornadoes.py
+++ CHAPPIE/tests/test_tornadoes.py
@@ -69,7 +69,7 @@ def test_process_tornadoes_aoi(test_get_tornadoes):
     actual = tornadoes.process_tornadoes(test_get_tornadoes, aoi_gdf)
     
     # save for now (sorted so expeccted doesn't have to be)
-    actual.sort_values(by=['geometry', 'wid', 'Date'], inplace=True, ignore_index=True)
+    actual.sort_values(by=['geometry', 'TornNo', 'Date'], inplace=True, ignore_index=True)
     #actual.to_file(os.path.join(EXPECTED_DIR, 'process_tornaodes_aoi.shp'))
     
     # check columns
@@ -80,8 +80,8 @@ def test_process_tornadoes_aoi(test_get_tornadoes):
     # assert no changes
     expected_file = os.path.join(EXPECTED_DIR, 'process_tornaodes_aoi.shp')
     expected = geopandas.read_file(expected_file)
-    expected.sort_values(by=['geometry', 'wid', 'Date'], inplace=True, ignore_index=True)
-    assert_geodataframe_equal(actual, expected, check_like=True)
+    expected.sort_values(by=['geometry', 'TornNo', 'Date'], inplace=True, ignore_index=True)
+    assert_geodataframe_equal(actual, expected, check_like=True, check_less_precise=True)
     
     # check rows
     #expected_file = os.path.join(EXPECTED_DIR, '1950-2022-torn-aspath.shp')
