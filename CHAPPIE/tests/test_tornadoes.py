c7857e795d26c584e1d29b4524104cf30d79fa18
Bousquin <Bousquin.Justin@epa.gov>
2024-03-20 09:36:07 -0500

    Clean up test script now that it is passing.

--- CHAPPIE/tests/test_tornadoes.py
+++ CHAPPIE/tests/test_tornadoes.py
@@ -52,13 +52,13 @@ def test_get_tornadoes():
     actual = tornadoes.get_tornadoes(aoi_gdf)
     
     # save for test (sorted so expeccted doesn't have to be)
-    actual.sort_values(by=['geometry', 'date'], inplace=True, ignore_index=True)
+    actual = actual.sort_values(by=['geometry', 'date'], ignore_index=True)
     #actual.to_file(os.path.join(TEST_DIR, 'get_tornaodes_aoi.shp'))
     
     # assert no changes
     expected_file = os.path.join(EXPECTED_DIR, 'get_tornaodes_aoi.shp')
     expected = geopandas.read_file(expected_file)
-    expected.sort_values(by=['geometry', 'date'], inplace=True, ignore_index=True)
+    expected = expected.sort_values(by=['geometry', 'date'], ignore_index=True)
 
     assert_geodataframe_equal(actual, expected)
     
@@ -69,7 +69,7 @@ def test_process_tornadoes_aoi(test_get_tornadoes):
     actual = tornadoes.process_tornadoes(test_get_tornadoes, aoi_gdf)
     
     # save for now (sorted so expeccted doesn't have to be)
-    actual.sort_values(by=['TornNo', 'Date'], inplace=True, ignore_index=True)
+    actual = actual.sort_values(by=['TornNo', 'Date'], ignore_index=True)
     #actual.to_file(os.path.join(EXPECTED_DIR, 'process_tornaodes_aoi.shp'))
     
     # check columns
@@ -77,17 +77,11 @@ def test_process_tornadoes_aoi(test_get_tornadoes):
     missing_cols = set(expected_cols) - set(actual.columns)
     assert not missing_cols, f"Columns missing: {', '.join(missing_cols)}"
     
-    #print debug
-    print(actual.iloc[6])
-    print(str(actual.iloc[6]['geometry']))
-    
     # assert no changes
     expected_file = os.path.join(EXPECTED_DIR, 'process_tornaodes_aoi.shp')
     expected = geopandas.read_file(expected_file)
-    expected.sort_values(by=['TornNo', 'Date'], inplace=True, ignore_index=True)
-    assert_geodataframe_equal(actual, expected, check_like=True, check_less_precise=True)
-    
-    # check rows
-    #expected_file = os.path.join(EXPECTED_DIR, '1950-2022-torn-aspath.shp')
-    #expected = geopandas.read_file(expected_file)
-    #assert(len(actual)==len(expected)), f'{len(actual)}!={len(expected)}'
+    expected = expected.sort_values(by=['TornNo', 'Date'], ignore_index=True)
+    assert_geodataframe_equal(actual,
+                              expected,
+                              check_like=True,
+                              check_less_precise=True)
