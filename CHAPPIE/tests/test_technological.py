9028c8b876ab3fc543a381127399a89fa6e8ce41
Thera Lombardi <lombardi.thera@epa.gov>
2024-06-07 10:42:58 -0400

    Run get_tri and save result as tests/expected/get_tri.parquet. Add test in test/test_technological.py. Also, update get_superfund function to drop OBJECTID columns that were causing errors in other tests. Update the expected get_superfund.parquet file to reflect the dropped OBJECTIDs too.

--- CHAPPIE/tests/test_technological.py
+++ CHAPPIE/tests/test_technological.py
@@ -22,6 +22,7 @@ aoi_gdf = geopandas.read_file(AOI)
 
 def test_get_superfund():
     actual = technological.get_superfund_npl(aoi_gdf)
+    actual.drop(columns=['OBJECTID'], inplace=True)
 
     # assert no changes
     expected_file = os.path.join(EXPECTED_DIR, 'get_superfund.parquet')
@@ -54,8 +55,7 @@ def test_get_landfills():
 def test_get_tri():
     actual = technological.get_tri(aoi_gdf)
     actual.drop(columns=['OBJECTID'], inplace=True)
-    #actual.sort_values(by=['GlobalID', 'geometry', 'Company'], inplace=True, ignore_index=True)
-    #actual.to_parquet(os.path.join(EXPECTED_DIR, 'get_tri.parquet'))
+    actual.sort_values(by=['EPA_REGISTRY_ID', 'geometry', 'FACILITY_NAME'], inplace=True, ignore_index=True)
  
     # assert no changes
     expected_file = os.path.join(EXPECTED_DIR, 'get_tri.parquet')
