b41f12e9722d98cd88a40bc8b87a95274bd2469d
Thera Lombardi <lombardi.thera@epa.gov>
2024-06-24 10:31:34 -0400

    Create health.py module, Create get_hospital function with doc-string. Run function and save results as parquet file. Add test and pass test.

---
+++ CHAPPIE/tests/test_health_assets.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Test technological 
+
+@author: tlomba01
+"""
+import os
+import geopandas
+from geopandas.testing import assert_geodataframe_equal
+from CHAPPIE.assets import health
+import pytest
+
+# CI inputs/expected
+DIRPATH = os.path.dirname(os.path.realpath(__file__))
+
+EXPECTED_DIR = os.path.join(DIRPATH, 'expected')  # Expected
+DATA_DIR = os.path.join(DIRPATH, 'data')  # inputs
+TEST_DIR = os.path.join(DIRPATH, 'results')  # test results (have to create)
+
+AOI = os.path.join(DATA_DIR, "BreakfastPoint_ServiceArea.shp")
+aoi_gdf = geopandas.read_file(AOI)
+
+def test_get_hospitals():
+    actual = health.get_hospitals(aoi_gdf)
+    actual.drop(columns=['FID'], inplace=True)
+    actual.sort_values(by=['USER_Facil', 'geometry', 'USER_Hospi'], inplace=True, ignore_index=True)
+
+    # assert no changes
+    expected_file = os.path.join(EXPECTED_DIR, 'get_hospitals.parquet')
+    expected = geopandas.read_parquet(expected_file)
+
+    assert_geodataframe_equal(actual, expected)
