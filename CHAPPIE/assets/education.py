236921360b0a658f37e75253e635a24eb45a9419
Thera Lombardi <lombardi.thera@epa.gov>
2024-08-23 13:26:59 -0400

    Get private schools (function, doc string, results, test).

--- CHAPPIE/assets/education.py
+++ CHAPPIE/assets/education.py
@@ -5,7 +5,7 @@ Module for education assets.
 """
 from CHAPPIE import layer_query
 
-def get_schools_public(base_url, aoi):
+def get_schools_public(aoi):
     """Get Public School locations within AOI.
 
     Parameters
@@ -19,7 +19,8 @@ def get_schools_public(base_url, aoi):
         GeoDataFrame for Public School locations.
 
     """
-
+    
+    base_url = "https://services1.arcgis.com/Hp6G80Pky0om7QvQ/arcgis/rest/services/"
     url = f"{base_url}/Public_Schools/FeatureServer"
     xmin, ymin, xmax, ymax = aoi.total_bounds
     bbox = [xmin, ymin, xmax, ymax]
@@ -29,3 +30,27 @@ def get_schools_public(base_url, aoi):
                                 layer=0,
                                 in_crs=aoi.crs.to_epsg())
 
+def get_schools_private(aoi):
+    """Get Private School locations within AOI.
+
+    Parameters
+    ----------
+    aoi : geopandas.GeoDataFrame
+        Spatial definition for Area Of Interest (AOI).
+
+    Returns
+    -------
+    geopandas.GeoDataFrame
+        GeoDataFrame for Private School locations.
+
+    """
+
+    base_url = "https://services1.arcgis.com/Hp6G80Pky0om7QvQ/arcgis/rest/services/"
+    url = f"{base_url}/Private_Schools/FeatureServer"
+    xmin, ymin, xmax, ymax = aoi.total_bounds
+    bbox = [xmin, ymin, xmax, ymax]
+    
+    return layer_query.get_bbox(aoi=bbox,
+                                url=url,
+                                layer=0,
+                                in_crs=aoi.crs.to_epsg())
--- CHAPPIE/tests/test_edu_assets.py
+++ CHAPPIE/tests/test_edu_assets.py
@@ -19,15 +19,27 @@ TEST_DIR = os.path.join(DIRPATH, 'results')  # test results (have to create)
 
 AOI = os.path.join(DATA_DIR, "BreakfastPoint_ServiceArea.shp")
 aoi_gdf = geopandas.read_file(AOI)
-base_url = "https://services1.arcgis.com/Hp6G80Pky0om7QvQ/arcgis/rest/services/"
+
 
 def test_get_schools_public():
-    actual = education.get_schools_public(base_url, aoi_gdf)
+    actual = education.get_schools_public(aoi_gdf)
     actual.drop(columns=['OBJECTID'], inplace=True)
     actual.sort_values(by=['NCESID', 'geometry', 'NAME'], inplace=True, ignore_index=True)
 
     # assert no changes
     expected_file = os.path.join(EXPECTED_DIR, 'schools_public.parquet')
+    expected = geopandas.read_parquet(expected_file)
+    
+    assert_geodataframe_equal(actual, expected)
+
+def test_get_schools_private():
+    actual = education.get_schools_private(aoi_gdf)
+    actual.drop(columns=['OBJECTID'], inplace=True)
+    actual.sort_values(by=['NCESID', 'geometry', 'NAME'], inplace=True, ignore_index=True)
+    #actual.to_parquet(os.path.join(EXPECTED_DIR, 'schools_private.parquet'))
+
+    # assert no changes
+    expected_file = os.path.join(EXPECTED_DIR, 'schools_private.parquet')
     expected = geopandas.read_parquet(expected_file)
     
     assert_geodataframe_equal(actual, expected)
