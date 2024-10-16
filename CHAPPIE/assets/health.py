1dfd32d2b8c70bcebbd0d206e0880301667d118e
Bousquin <Bousquin.Justin@epa.gov>
2024-10-16 10:50:45 -0500

    init commit - health.get_providers(), uses 5-digit zipcode to retrieve providers from NPI API 200 results at a time. Logic may need some work. Likewise this returns a lot slowly so we might want to consider limiting the query vs filtering results after (discussion on issue).

--- CHAPPIE/assets/health.py
+++ CHAPPIE/assets/health.py
@@ -3,7 +3,11 @@ Module for health assets.
 
 @author: tlomba01
 """
+import requests
+import pandas
 from CHAPPIE import layer_query
+
+_npi_url = "https://npiregistry.cms.hhs.gov/api"
 
 def get_hospitals(aoi):
     """Get Hospital locations within AOI.
@@ -52,3 +56,45 @@ def get_urgent_care(aoi):
                                 url=url,
                                 layer=0,
                                 in_crs=aoi.crs.to_epsg())
+
+
+# def _paged_get(params, i=0, dfs=[]):
+#     if i>0:
+#         params["skip"]=i
+#     res = requests.get(_npi_url, params)
+#     if res.ok:
+#         df = pandas.DataFrame(res.json()['results'])
+#         if res.json()['result_count']==200:
+#             _paged_get(params, i+=200, dfs.append(df))
+#         else:
+#             return dfs.append(df)
+
+#     return pandas.concat(dfs.
+
+
+def get_providers(aoi):
+    zips = layer_query.getZipCode(aoi)
+    params = {"version": 2.1, "limit": 200}
+    dfs = []
+    for zip in zips:
+        i=0
+        new_results=True
+        params['postal_code']=zip
+        #dfs.append(_paged_get(params))
+        while new_results:
+            params["skip"]=i
+            res = requests.get(_npi_url, params)
+            res.raise_for_status()
+            #if res.ok:
+            df= pandas.DataFrame(res.json()['results'])
+            df["zip5"]=zip  # Add 5-digit zipcode to show retrieval set
+            dfs.append(df)
+            if res.json()['result_count']==200:
+                if i>2000:
+                    break
+                else:
+                    new_results = True
+                    i+=200
+            else:
+                new_results = False
+    return pandas.concat(dfs)
