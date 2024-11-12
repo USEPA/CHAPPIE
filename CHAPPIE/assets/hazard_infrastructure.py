19bb547578dac2ca8c72a1aaac230223e5cfb6c2
Thera Lombardi <lombardi.thera@epa.gov>
2024-11-12 11:17:06 -0500

    Run function and save result as tests/expected/dams.parquet. Add test in tests/test_infrastructure_assets.py.

--- CHAPPIE/assets/hazard_infrastructure.py
+++ CHAPPIE/assets/hazard_infrastructure.py
@@ -19,3 +19,12 @@ def get_dams(aoi):
         GeoDataFrame for dam locations.
 
     """
+
+    url = 'https://services.arcgis.com/xOi1kZaI0eWDREZv/ArcGIS/rest/services/NTAD_Dams/FeatureServer'
+    xmin, ymin, xmax, ymax = aoi.total_bounds
+    bbox = [xmin, ymin, xmax, ymax]
+    
+    return layer_query.get_bbox(aoi=bbox,
+                                url=url,
+                                layer=0,
+                                in_crs=aoi.crs.to_epsg())
