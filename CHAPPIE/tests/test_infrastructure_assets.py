9be0323ac24c6ac7b20e14e770c98cea62023dbc
Thera Lombardi <lombardi.thera@epa.gov>
2024-11-14 12:56:14 -0500

    Use a different AOI (Little Pine Island) to capture at least one leveed area.

--- CHAPPIE/tests/test_infrastructure_assets.py
+++ CHAPPIE/tests/test_infrastructure_assets.py
@@ -17,7 +17,9 @@ EXPECTED_DIR = os.path.join(DIRPATH, 'expected')  # Expected
 DATA_DIR = os.path.join(DIRPATH, 'data')  # inputs
 
 AOI = os.path.join(DATA_DIR, "BreakfastPoint_ServiceArea.shp")
+AOI2 = os.path.join(DATA_DIR, "LittlePineIsland_ServiceArea.shp")
 aoi_gdf = geopandas.read_file(AOI)
+aoi_gdf2 = geopandas.read_file(AOI2)
 
 
 def test_get_dams():
@@ -31,6 +33,12 @@ def test_get_dams():
     assert_geodataframe_equal(actual, expected)
 
 def test_get_levees():
-    actual = hazard_infrastructure.get_levee(aoi_gdf)    
+    actual = hazard_infrastructure.get_levee(aoi_gdf2)    
     assert isinstance(actual, geopandas.geodataframe.GeoDataFrame)
-    assert len(actual)==0
+    actual.drop(columns=['OBJECTID'], inplace=True)
+    actual.sort_values(by=['SYSTEM_ID', 'SYSTEM_NAME'], inplace=True, ignore_index=True)
+    
+    expected_file = os.path.join(EXPECTED_DIR, 'levees.parquet')
+    expected = geopandas.read_parquet(expected_file)
+ 
+    assert_geodataframe_equal(actual, expected)
