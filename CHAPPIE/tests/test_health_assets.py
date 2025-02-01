2fdbef14b48d36077f560115f13d36e36cd53d00
Justin Bousquin <bousquin.justin@epa.gov>
2025-01-31 18:28:16 -0600

    Update test_health_assets.py tests_get_provider expected len

--- CHAPPIE/tests/test_health_assets.py
+++ CHAPPIE/tests/test_health_assets.py
@@ -93,7 +93,7 @@ def test_get_providers(providers: pandas.DataFrame):
     actual_len = [len(providers[providers['zip5']==zip]) for zip in expected]
     #expected_len = [1069, 311, 1841, 398, 149, 26, 168, 306, 44, 27]
     #expected_len = [1071, 311, 1841, 398, 149, 26, 168, 306, 44, 27]
-    expected_len = [1074, 313, 1847, 411, 155, 27, 171, 307, 46, 29]
+    expected_len = [1074, 313, 1847, 412, 155, 27, 171, 307, 46, 29]
     assert actual_len==expected_len
 
     # Test result dataframes
