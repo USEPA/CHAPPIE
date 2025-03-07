5431740a2d0cc96a87d4b585bb3d4ad4b01945bd
Justin Bousquin <Bousquin.justin@epa.gov>
2025-03-07 16:33:40 -0600

    Drop files/locations not used in these tests

--- CHAPPIE/tests/test_transit_assets.py
+++ CHAPPIE/tests/test_transit_assets.py
@@ -16,13 +16,10 @@ DIRPATH = os.path.dirname(os.path.realpath(__file__))
 
 EXPECTED_DIR = os.path.join(DIRPATH, 'expected')  # Expected
 DATA_DIR = os.path.join(DIRPATH, 'data')  # inputs
-TEST_DIR = os.path.join(DIRPATH, 'results')  # test results (have to create)
-PARCEL_DIR = os.path.join(EXPECTED_DIR, 'parcels')
 
 AOI = os.path.join(DATA_DIR, "BreakfastPoint_ServiceArea.shp")
-PARCELS = os.path.join(PARCEL_DIR, "BP_Regrid.shp")
 aoi_gdf = geopandas.read_file(AOI)
-parcels_gdf = geopandas.read_file(PARCELS)
+
 
 def test_get_air():
     actual = transit.get_air(aoi_gdf)
@@ -48,7 +45,6 @@ def test_get_bus():
 
 def test_get_rail():
     actual = transit.get_rail(aoi_gdf)
-    #actual.drop(columns=['OBJECTID'], inplace=True)
     #actual.to_parquet(os.path.join(EXPECTED_DIR, 'get_rail.parquet'))
 
     # assert no changes
