a134965139ed4923d2537916778eaa116d10c67e
Thera Lombardi <lombardi.thera@epa.gov>
2025-03-07 12:05:46 -0500

    Create function. Update doc string.

---
+++ CHAPPIE/assets/recreation.py
@@ -0,0 +1,33 @@
+"""
+Module for recreation assets
+
+@author: tlomba01
+"""
+import geopandas
+from numpy import nan
+from CHAPPIE import layer_query
+
+
+def get_padus(aoi):
+    """Get protected area sites within AOI.
+
+    Parameters
+    ----------
+    aoi : geopandas.GeoDataFrame
+        Spatial definition for Area Of Interest (AOI).
+
+    Returns
+    -------
+    geopandas.GeoDataFrame
+        GeoDataFrame for protected area sites from PADUS.
+
+    """
+
+    url = 'https://services.arcgis.com/v01gqwM5QqNysAAi/ArcGIS/rest/services/PADUS_Public_Access/FeatureServer'
+    xmin, ymin, xmax, ymax = aoi.total_bounds
+    bbox = [xmin, ymin, xmax, ymax]
+
+    return layer_query.get_bbox(aoi=bbox,
+                                url=url,
+                                layer=0,
+                                in_crs=aoi.crs.to_epsg())
