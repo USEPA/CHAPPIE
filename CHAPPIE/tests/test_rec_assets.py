bfbed2f955845ff1917d367e3a8174689bbbd64a
Thera Lombardi <lombardi.thera@epa.gov>
2025-03-10 16:31:38 -0400

    Add PADUS parquet. Update test function.

--- CHAPPIE/tests/test_rec_assets.py
+++ CHAPPIE/tests/test_rec_assets.py
@@ -15,13 +15,13 @@ DIRPATH = os.path.dirname(os.path.realpath(__file__))
 EXPECTED_DIR = os.path.join(DIRPATH, 'expected')  # Expected
 DATA_DIR = os.path.join(DIRPATH, 'data')  # inputs
 
-AOI = os.path.join(DATA_DIR, "BreakfastPoint_ServiceArea.shp")
-aoi_gdf = geopandas.read_file(AOI)
+AOI_MIT_BANK = os.path.join(DATA_DIR, "BreakfastPoint_RIBITS2020.shp")
+aoi_gdf = geopandas.read_file(AOI_MIT_BANK)
 
 def test_get_padus():
     actual = recreation.get_padus(aoi_gdf)
-    #actual.drop(columns=['OBJECTID'], inplace=True)
-    #actual.sort_values(by=['CertDate', 'RESNAME', 'geometry'], inplace=True, ignore_index=True)
+    actual.drop(columns=['OBJECTID'], inplace=True)
+    actual.sort_values(by=['Unit_Nm', 'geometry'], inplace=True, ignore_index=True)
     #actual.to_parquet(os.path.join(EXPECTED_DIR, 'padus.parquet'))
 
     # assert no changes
@@ -29,4 +29,5 @@ def test_get_padus():
     expected = geopandas.read_parquet(expected_file)
  
     assert_geodataframe_equal(actual, 
-                              expected)
+                              expected,
+                              check_less_precise=True)
