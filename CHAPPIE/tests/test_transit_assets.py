c6611c19a7cfeadfad8afb33e123ab4e19be7efd
AA\edamico <damico.ellen@epa.gov>
2025-03-10 16:16:44 -0400

    Added sort fields for air and bus to try

--- CHAPPIE/tests/test_transit_assets.py
+++ CHAPPIE/tests/test_transit_assets.py
@@ -24,6 +24,10 @@ aoi_gdf = geopandas.read_file(AOI)
 def test_get_air():
     actual = transit.get_air(aoi_gdf)
     actual.drop(columns=['OBJECTID'], inplace=True)
+    actual.rename(columns={'GEOMETRY': 'geometry'}, inplace=True)
+    actual.sort_values(by=['ARPT_ID', 'geometry', 'ARPT_NAME'],
+                       inplace=True,
+                       ignore_index=True)
     #actual.to_parquet(os.path.join(EXPECTED_DIR, 'get_air.parquet'))
 
     # assert no changes
@@ -38,6 +42,11 @@ def test_get_air():
 def test_get_bus():
     actual = transit.get_bus(aoi_gdf)
     actual.drop(columns=['OBJECTID'], inplace=True)
+    actual.rename(columns={'GEOMETRY': 'geometry'}, inplace=True)
+    sortList = ["ntd_id","stop_id","stop_name", 'geometry']
+    actual.sort_values(by=sortList,
+                       inplace=True,
+                       ignore_index=True)
     #actual.to_parquet(os.path.join(EXPECTED_DIR, 'get_bus.parquet'))
 
     # assert no changes
@@ -51,6 +60,7 @@ def test_get_bus():
 
 def test_get_rail():
     actual = transit.get_rail(aoi_gdf)
+    actual.rename(columns={'GEOMETRY': 'geometry'}, inplace=True)
     #actual.to_parquet(os.path.join(EXPECTED_DIR, 'get_rail.parquet'))
 
     # assert no changes
