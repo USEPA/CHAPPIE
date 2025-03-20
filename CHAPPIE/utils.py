98b6e79e975af77d3c9363af471046dd582a05aa
Thera Lombardi <lombardi.thera@epa.gov>
2025-03-20 11:32:05 -0400

    Prep function.

--- CHAPPIE/utils.py
+++ CHAPPIE/utils.py
@@ -4,9 +4,34 @@
 @author: tlomba01
 """
 import os
+from CHAPPIE import layer_query
 
 _regrid_base_url = "https://fs.regrid.com/"
+_regrid_fs_path = "/rest/services/premium/FeatureServer"
 
-# get key from env
-_regrid_api_key = os.environ['REGRID_API_KEY']
-_regrid_fs_path = "/rest/services/premium/FeatureServer/0"
+def get_regrid(aoi, api_key=None):
+    """Get Regrid parcels within AOI.
+
+    Parameters
+    ----------
+    aoi : geopandas.GeoDataFrame
+        Spatial definition for Area Of Interest (AOI).
+
+    Returns
+    -------
+    geopandas.GeoDataFrame
+        GeoDataFrame for Hospital locations.
+
+    """
+    
+    if api_key == None:
+        api_key = os.environ['REGRID_API_KEY']
+    url = f"{_regrid_base_url}{api_key}{_regrid_fs_path}"
+    xmin, ymin, xmax, ymax = aoi.total_bounds
+    bbox = [xmin, ymin, xmax, ymax]
+
+    return layer_query.get_bbox(aoi=bbox,
+                                url=url,
+                                layer=0,
+                                out_fields=['parcelnumb'],
+                                in_crs=aoi.crs.to_epsg())
