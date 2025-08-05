d78aa394a10ac9154e4a316d5acbd918136232e4
AA\edamico <damico.ellen@epa.gov>
2025-08-05 13:47:23 -0400

    Updated test_wq to use Somerset_30mBuffer. Test passed. :)

--- CHAPPIE/tests/test_wq.py
+++ CHAPPIE/tests/test_wq.py
@@ -17,45 +17,45 @@ DIRPATH = os.path.dirname(os.path.realpath(__file__))
 EXPECTED_DIR = os.path.join(DIRPATH, 'expected')  # Expected
 DATA_DIR = os.path.join(DIRPATH, 'data')  # inputs
 
-AOI_MIT_BANK = os.path.join(DATA_DIR, "BreakfastPoint_RIBITS2020.shp")
-AOI_MIT_BANK_poly = os.path.join(DATA_DIR, "BreakfastPoint_ServiceArea.shp")
+AOI_MIT_BANK = os.path.join(DATA_DIR, "Somerset_30mBuffer.shp")
+AOI_MIT_BANK_poly = os.path.join(DATA_DIR, "Somerset_30mBuffer.shp")
 aoi_gdf = geopandas.read_file(AOI_MIT_BANK)
 aoi_gdf_poly = geopandas.read_file(AOI_MIT_BANK_poly)
 
-# def test_get_attains_points():
-#     actual = wq.get_attains_points(aoi_gdf)
-#     actual.drop(columns=['OBJECTID'], inplace=True)
-#     actual.sort_values(by=['assessmentunitidentifier', 'geometry'], inplace=True, ignore_index=True)
-#     #actual.to_parquet(os.path.join(EXPECTED_DIR, 'attains_points.parquet'))
+def test_get_attains_points():
+    actual = wq.get_attains_points(aoi_gdf)
+    actual.drop(columns=['OBJECTID'], inplace=True)
+    actual.sort_values(by=['assessmentunitidentifier', 'geometry'], inplace=True, ignore_index=True)
+    #actual.to_parquet(os.path.join(EXPECTED_DIR, 'attains_points.parquet'))
 
-#     # assert no changes
-#     expected_file = os.path.join(EXPECTED_DIR, 'attains_points.parquet')
-#     expected = geopandas.read_parquet(expected_file)
+    # assert no changes
+    expected_file = os.path.join(EXPECTED_DIR, 'attains_points.parquet')
+    expected = geopandas.read_parquet(expected_file)
 
-#     assert_geodataframe_equal(actual,
-#                               expected,
-#                               check_less_precise=True)
+    assert_geodataframe_equal(actual,
+                              expected,
+                              check_less_precise=True)
 
 
-# def test_get_attains_lines():
-#     actual = wq.get_attains_lines(aoi_gdf)
-#     actual.drop(columns=['OBJECTID'], inplace=True)
-#     actual.sort_values(by=['assessmentunitidentifier', 'geometry'], inplace=True, ignore_index=True)
-#     #actual.to_parquet(os.path.join(EXPECTED_DIR, 'attains_lines.parquet'))
+def test_get_attains_lines():
+    actual = wq.get_attains_lines(aoi_gdf)
+    actual.drop(columns=['OBJECTID'], inplace=True)
+    actual.sort_values(by=['assessmentunitidentifier', 'geometry'], inplace=True, ignore_index=True)
+    #actual.to_parquet(os.path.join(EXPECTED_DIR, 'attains_lines.parquet'))
 
-#     # assert no changes
-#     expected_file = os.path.join(EXPECTED_DIR, 'attains_lines.parquet')
-#     expected = geopandas.read_parquet(expected_file)
+    # assert no changes
+    expected_file = os.path.join(EXPECTED_DIR, 'attains_lines.parquet')
+    expected = geopandas.read_parquet(expected_file)
 
-#     assert_geodataframe_equal(actual,
-#                               expected,
-#                               check_less_precise=True)
+    assert_geodataframe_equal(actual,
+                              expected,
+                              check_less_precise=True)
 
 def test_get_attains_polygons():
     actual = wq.get_attains_polygons(aoi_gdf_poly)
     actual.drop(columns=['OBJECTID'], inplace=True)
     actual.sort_values(by=['assessmentunitidentifier', 'geometry'], inplace=True, ignore_index=True)
-    actual.to_parquet(os.path.join(EXPECTED_DIR, 'attains_polygons.parquet'))
+   # actual.to_parquet(os.path.join(EXPECTED_DIR, 'attains_polygons.parquet'))
 
     # assert no changes
     expected_file = os.path.join(EXPECTED_DIR, 'attains_polygons.parquet')
