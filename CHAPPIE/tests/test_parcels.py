61e3e09217166f744e7d1d3e446988d1ef9bb912
tlomba01 <lombardi.thera@epa.gov>
2025-08-14 15:21:01 -0400

    Add test for process_regrid() that uses the result of get_regrid as input.

--- CHAPPIE/tests/test_parcels.py
+++ CHAPPIE/tests/test_parcels.py
@@ -3,9 +3,10 @@ Test get and process Regrid parcels.
 
 @author: tlomba01
 """
-# Commenting this out for now since I don't think we want to automate tests for this
+# #Commenting this out for now since I don't think we want to automate tests for this
 # import os
 # import geopandas
+# import pytest
 # from geopandas.testing import assert_geodataframe_equal
 # from CHAPPIE import parcels
 
@@ -22,17 +23,27 @@ Test get and process Regrid parcels.
 
 
 # # Expected file is a user arg input so the test doesn't run from retained results
-# def test_get_regrid(expected_file):
-#     actual = utils.get_regrid(aoi_gdf)
+# @pytest.fixture(scope='session')
+# def parcels_gdf():
+#     return parcels.get_regrid(aoi_gdf)
+
+# def test_get_regrid(parcels_gdf: geopandas.GeoDataFrame):
 #     #actual.drop(columns=['FID'], inplace=True)
-#     actual.sort_values(by=['geometry', 'geoid'], inplace=True, ignore_index=True)
-#     #actual.to_parquet(os.path.join(EXPECTED_DIR, 'get_regrid.parquet'))
+#     parcels_gdf.sort_values(by=['geometry', 'geoid'], inplace=True, ignore_index=True)
+#     #parcels_gdf.to_parquet(os.path.join(EXPECTED_DIR, 'get_regrid.parquet'))
 
 #     # assert no changes
-#     #expected_file = os.path.join(EXPECTED_DIR, 'get_regrid.parquet')
+#     expected_file = os.path.join(EXPECTED_DIR, 'get_regrid.parquet')
 
 #     expected = geopandas.read_parquet(expected_file)
 
 #     # First check that results are same lenth
-#     assert(len(actual)==len(expected)), f'{len(actual)}!={len(expected)}'
-#     assert_geodataframe_equal(actual, expected)
+#     assert(len(parcels_gdf)==len(expected)), f'{len(parcels_gdf)}!={len(expected)}'
+#     assert_geodataframe_equal(parcels_gdf, expected)
+    
+
+# def test_process_regrid(parcels_gdf: geopandas.GeoDataFrame):
+#     actual_centroids = parcels.process_regrid(parcels_gdf)
+#     assert(len(parcels_gdf)==len(actual_centroids))
+#     assert(len(actual_centroids[actual_centroids['geometry'].geom_type == 'Point'])==len(parcels_gdf))
+#     assert(len(actual_centroids[actual_centroids['geometry'].geom_type == 'Polygon'])==0)
