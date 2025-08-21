735103e9948a9dd1c20acfd167f29d200fead0da
AA\edamico <damico.ellen@epa.gov>
2025-08-21 13:10:58 -0400

    Updating with last version - still not complete

--- CHAPPIE/assets/recreationalAreas.py
+++ CHAPPIE/assets/recreationalAreas.py
@@ -7,6 +7,7 @@ Module for recreation areas
 import os
 import geopandas
 import requests
+import io
 from io import BytesIO
 #from CHAPPIE import layer_query
 import py7zr, re
@@ -19,15 +20,21 @@ DATA_DIR = os.path.join(DIRPATH, 'lyrpkg')  # inputs
 
 url = r"https://epa.maps.arcgis.com/sharing/rest/content/items/4f14ea9215d1498eb022317458437d19/data"
 
-class InMemoryFactory:
-        def __init__(self):
-            self.extracted_files = {}
+class InMemoryIOFactory:
+    def __init__(self):
+        self.files = {}
 
-        def create_io(self, filename):
-            # This method is called by py7zr to get an IO object for each file
-            in_memory_file = BytesIO()
-            self.extracted_files[filename] = in_memory_file
-            return in_memory_file
+    def create(self, filename):
+        # Create a BytesIO object for the file
+        bio = BytesIO()
+        self.files[filename] = bio
+        return bio
+
+    def close(self, bio):
+        # Optional: Perform any cleanup or finalization here
+        pass
+
+
 
 def download_unzip_lyrpkg(url, save_path):
     """Download and unzip recreation area layer packages from URL
@@ -47,43 +54,43 @@ def download_unzip_lyrpkg(url, save_path):
         #Download the file from `url` and save it locally under `save_path`
         # Send GET request to the URL
         response = requests.get(url)
-        
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            # Write the content of the response to a local file
-            with open(save_path, 'wb') as file:
-                file.write(response.content)
-            print(f"File downloaded successfully: {save_path}")
-        else:
-            print(f"Failed to download file. Status code: {response.status_code}")
-    
-        #Unzip the file using py7zr
-        #zip7file = save_path
-        # zip7file = BytesIO(response.content)
-        path = os.path.dirname(save_path) 
         folderList = []
-        with py7zr.SevenZipFile(BytesIO(response.content), mode='r') as z:
-                file_list = z.namelist()
-                
-                for filename in file_list:
-                    if filename.split('/')[0]  not in folderList: # Get the first part of the path
-                        folderList.append(filename.split('/')[0])
-        filter_pattern = re.compile(folderList[-1])
-        #dfList = []
-        factory = InMemoryFactory()
-        with py7zr.SevenZipFile( BytesIO(response.content), 'r') as archive:
-            allfiles = archive.getnames()
-            selective_files = [f for f in allfiles if filter_pattern.match(f)]
-            #dfList.append(geopandas.read_file(path + f'//{folderList[-1]}/recareas.gdb', layer="recareas", driver='OpenFileGDB'))
-            #archive.extract(path,targets=selective_files)
-            archive.extract(factory, targets=selective_files)
+        # Check if the request was successful (status code 200)
+        factory = InMemoryIOFactory()
+        if response.status_code == 200:
+            with py7zr.SevenZipFile(BytesIO(response.content), mode='r') as z:
+                    #get a list of all archived file names from the zip
+                    file_list = z.namelist()
+                    for filename in file_list:
+                        if filename.split('/')[0]  not in folderList: # Get the first part of the path
+                            folderList.append(filename.split('/')[0])
+                    #create a filter pattern to match the desired folder
+                    filter_pattern = re.compile(f'{folderList[-1]}/recareas.gdb')
+                    select_files = [f for f in file_list if filter_pattern.match(f)]
+                    #extract the selected files using the custom factory
+                    z.extract(targets=select_files, factory=factory)
+            
+            
 
-        # #convert the layer to a geodataframe
-        # if len(dfList) <= 1:
-        #     gdf = dfList[0]
-        
-        gdf = geopandas.read_file(path + f'//{folderList[-1]}/recareas.gdb', layer="recareas", driver='OpenFileGDB')
-        return gdf
+            #below code is not working - need to figure out how to read the in-memory gdb        
+            for filename, bio_object in factory.files():
+                print(filename)
+
+            for filename, bio_object in factory.files.items():
+                if filter_pattern.match(filename):
+                    print(f"Extracted file to memory: {filename}")
+                    # Seek to the beginning of the BytesIO object to read its content
+                    bio_object.seek(0)
+                    content = bio_object.read()
+                    # with content.open() as src:
+                    #     crs = src.crs
+                    #     gdf = geopandas.GeoDataFrame.from_features(src, crs=crs)
+                    #     print(gdf.head())
+                    gdf = geopandas.read_file(content, layer="recareas", driver='OpenFileGDB')
+            
+            #gdf = geopandas.read_file('in_memory/recareas.gdb', layer="recareas", driver='OpenFileGDB')
+            gdf = geopandas.read_file("OpenFileGDB:v108/recareas.gdb")
+            return gdf
     except Exception as e:
         print(f"Error: {e}")
 
@@ -130,3 +137,7 @@ def process_recreationalArea(recAreas_gdf, aoi):
     recAreas_aoi = recAreas_gdf.clip(aoi.total_bounds)
     
     return recAreas_aoi 
+
+
+
+
