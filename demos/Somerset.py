01b0d2b8ec1e62c39f733696c7f59f29ac8d431a
Justin Bousquin <Bousquin.justin@epa.gov>
2025-08-25 15:02:51 -0500

    Start by adding parcels and query with them

--- demos/Somerset.py
+++ demos/Somerset.py
@@ -9,6 +9,7 @@ import os
 import geopandas
 import pandas
 
+from CHAPPIE import parcels
 from CHAPPIE.assets import (
     cultural,
     education,
@@ -17,6 +18,7 @@ from CHAPPIE.assets import (
     hazard_infrastructure,
     health,
 )
+
 #from CHAPPIE.endpoints import hazard_losses
 from CHAPPIE.hazards import flood, technological, tornadoes, tropical_cyclones, weather
 from CHAPPIE.household import svi
@@ -34,70 +36,74 @@ house_dict = {}
 # API Key handling
 usda_API = os.environ['usda_API']
 
+# Get intersecting parcels
+parcel_gdf = parcels.get_regrid(aoi_gdf)
+parcel_centroids =  parcels.process_regrid(parcel_gdf)
+
 # Get cultural assets
-assets_dict["historic_sites"] = cultural.get_historic(aoi_gdf)
-assets_dict["libraries"] = cultural.get_library(aoi_gdf)
-assets_dict["museums"] = cultural.get_museums(aoi_gdf)
-assets_dict["worship"] = cultural.get_worship(aoi_gdf)
+assets_dict["historic_sites"] = cultural.get_historic(parcel_gdf)
+assets_dict["libraries"] = cultural.get_library(parcel_gdf)
+assets_dict["museums"] = cultural.get_museums(parcel_gdf)
+assets_dict["worship"] = cultural.get_worship(parcel_gdf)
 
 # Get educational assets
-assets_dict["schools_public"] = education.get_schools_public(aoi_gdf)
-assets_dict["schools_private"] = education.get_schools_private(aoi_gdf)
-assets_dict["child_care"] = education.get_child_care(aoi_gdf)
-assets_dict["colleges_uni"] = education.get_colleges_universities(aoi_gdf)
-assets_dict["colleges_sup"] = education.get_supplemental_colleges(aoi_gdf)
+assets_dict["schools_public"] = education.get_schools_public(parcel_gdf)
+assets_dict["schools_private"] = education.get_schools_private(parcel_gdf)
+assets_dict["child_care"] = education.get_child_care(parcel_gdf)
+assets_dict["colleges_uni"] = education.get_colleges_universities(parcel_gdf)
+assets_dict["colleges_sup"] = education.get_supplemental_colleges(parcel_gdf)
 
 # Get emergency assets
-assets_dict["fire_ems"] = emergency.get_fire_ems(aoi_gdf)
-assets_dict["police"] = emergency.get_police(aoi_gdf)
+assets_dict["fire_ems"] = emergency.get_fire_ems(parcel_gdf)
+assets_dict["police"] = emergency.get_police(parcel_gdf)
 
 # Get food assets
-assets_dict["agritourism"] = food.get_agritourism(aoi_gdf, usda_API)
-assets_dict["csa"] = food.get_CSA(aoi_gdf, usda_API)
-assets_dict["farmers_market"] = food.get_farmers_market(aoi_gdf, usda_API)
-assets_dict["food_hub"] = food.get_food_hub(aoi_gdf, usda_API)
-assets_dict["farm_store"] = food.get_farm_store(aoi_gdf, usda_API)
+assets_dict["agritourism"] = food.get_agritourism(parcel_gdf, usda_API)
+assets_dict["csa"] = food.get_CSA(parcel_gdf, usda_API)
+assets_dict["farmers_market"] = food.get_farmers_market(parcel_gdf, usda_API)
+assets_dict["food_hub"] = food.get_food_hub(parcel_gdf, usda_API)
+assets_dict["farm_store"] = food.get_farm_store(parcel_gdf, usda_API)
 
 # Get health assets
-assets_dict["hospitals"] = health.get_hospitals(aoi_gdf)
-#assets_dict["urgent_care"] = health.get_urgent_care(aoi_gdf)
-#providers = health.get_providers(aoi_gdf)
+assets_dict["hospitals"] = health.get_hospitals(parcel_gdf)
+#assets_dict["urgent_care"] = health.get_urgent_care(parcel_gdf)
+#providers = health.get_providers(parcel_gdf)
 #assets_dict["providers"] = health.provider_address(providers)
 
 # Get hazard infrastructure assets
-assets_dict["dams"] = hazard_infrastructure.get_dams(aoi_gdf)
-assets_dict["levees"] = hazard_infrastructure.get_levee(aoi_gdf)
+assets_dict["dams"] = hazard_infrastructure.get_dams(parcel_gdf)
+assets_dict["levees"] = hazard_infrastructure.get_levee(parcel_gdf)
 
 # Get flood hazard
-hazards_dict["flood_FEMA"] = flood.get_fema_nfhl(aoi_gdf)
+hazards_dict["flood_FEMA"] = flood.get_fema_nfhl(parcel_gdf)
 #TODO: requires parcels first
 # flood.get_flood()
 
 # Get hazards
 in_crs = 'ESRI:102005'
-tornadoes_gdf = tornadoes.get_tornadoes(aoi_gdf.to_crs(in_crs))
-hazards_dict["tornadoes"] = tornadoes.process_tornadoes(tornadoes_gdf,
-                                                        aoi_gdf.to_crs(in_crs))
-cyclones = tropical_cyclones.get_cyclones(aoi_gdf.to_crs('ESRI:102005'))
+tornadoes_gdf = tornadoes.get_tornadoes(parcel_gdf.to_crs(in_crs))
+hazards_dict["tornadoes"] = tornadoes.process_tornadoes(parcel_gdf,
+                                                        parcel_gdf.to_crs(in_crs))
+cyclones = tropical_cyclones.get_cyclones(parcel_gdf.to_crs('ESRI:102005'))
 hazards_dict["tropical_cyclones"] = tropical_cyclones.process_cyclones(cyclones,
-                                                                       aoi_gdf.to_crs(in_crs))
+                                                                       parcel_gdf.to_crs(in_crs))
 
 # Get weather hazards
-hazards_dict["heat"] = weather.get_heat_events(aoi_gdf)
+hazards_dict["heat"] = weather.get_heat_events(parcel_gdf)
 
 # Get tech hazards
-hazards_dict["superfund"] = technological.get_superfund_npl(aoi_gdf)
-hazards_dict["brownfields"] = technological.get_FRS_ACRES(aoi_gdf)
-hazards_dict["landfills"] = technological.get_landfills(aoi_gdf)
-hazards_dict["tri"] = technological.get_tri(aoi_gdf)
+hazards_dict["superfund"] = technological.get_superfund_npl(parcel_gdf)
+hazards_dict["brownfields"] = technological.get_FRS_ACRES(parcel_gdf)
+hazards_dict["landfills"] = technological.get_landfills(parcel_gdf)
+hazards_dict["tri"] = technological.get_tri(parcel_gdf)
 
 # Get hazard endpoints
 #hazards_dict["losses"] = hazard_losses.get_hazard_losses
 
 # Get svi metrics
 house_dict["svi"] = svi.get_SVI('12033',
-                                        level='block group',
-                                        year=2022)
+                                level='block group',
+                                year=2022)
 
 #TODO: write QA txt
 cols = ["Table", "Column", "data_type", "min", "max", "mean/mode", "NaN_Count"]
