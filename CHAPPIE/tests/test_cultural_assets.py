90d6a01595d65bab34fd021227a9877842c5296f
Ellen D'Amico <damico.ellen@epa.gov>
2025-08-25 17:08:41 -0400

    Update test_cultural_assets.py
    Updated with recreation area tests

--- CHAPPIE/tests/test_cultural_assets.py
+++ CHAPPIE/tests/test_cultural_assets.py
@@ -2,7 +2,7 @@
 """
 Test flood
 
-@author: tlomba01, jbousquin
+@author: tlomba01, jbousquin, edamico
 """
 import os
 
@@ -18,7 +18,9 @@ EXPECTED_DIR = os.path.join(DIRPATH, 'expected')  # Expected
 DATA_DIR = os.path.join(DIRPATH, 'data')  # inputs
 
 AOI = os.path.join(DATA_DIR, "BreakfastPoint_ServiceArea.shp")
+AOI_MIT_BANK_SA = os.path.join(DATA_DIR, "BreakfastPoint_ServiceArea.shp")
 aoi_gdf = geopandas.read_file(AOI)
+aoi_gdf_sa = geopandas.read_file(AOI_MIT_BANK_SA)
 
 def test_get_historic():
     actual = cultural.get_historic(aoi_gdf)
@@ -70,3 +72,17 @@ def test_get_worship():
     expected = geopandas.read_parquet(expected_file)
 
     assert_geodataframe_equal(actual, expected)
+
+
+def test_get_recAreas():
+    actual = cultural.process_recreationalArea(aoi_gdf_sa)
+    actual.sort_values(by=['NAME', 'geometry'], inplace=True, ignore_index=True)
+    actual.to_parquet(os.path.join(EXPECTED_DIR, 'recArea.parquet'))
+
+    # assert no changes
+    expected_file = os.path.join(EXPECTED_DIR, 'recArea.parquet')
+    expected = geopandas.read_parquet(expected_file)
+
+    assert_geodataframe_equal(actual,
+                              expected,
+                              check_less_precise=True)
