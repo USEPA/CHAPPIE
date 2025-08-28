a5f9e29ef3da6177f3cf0937d021287989b15578
Justin Bousquin <Bousquin.justin@epa.gov>
2025-08-28 10:45:15 -0500

    Drop column (Effective Date) from test for get_air

--- CHAPPIE/tests/test_transit_assets.py
+++ CHAPPIE/tests/test_transit_assets.py
@@ -23,7 +23,8 @@ aoi_gdf = geopandas.read_file(AOI)
 
 def test_get_air():
     actual = transit.get_air(aoi_gdf)
-    actual.drop(columns=['OBJECTID'], inplace=True)
+    # Effective Date (EFF_DATE) is updated when info is verified
+    actual.drop(columns=['OBJECTID', "EFF_DATE"], inplace=True)
     # Drop geometry redundant columns that cause trouble
     actual.drop(columns=["LAT_DEG", "LONG_DEG", 'LAT_MIN', 'LONG_MIN'],
                 inplace=True)
--- CHAPPIE/tests/update_expected.py
+++ CHAPPIE/tests/update_expected.py
@@ -173,7 +173,7 @@ from CHAPPIE.assets import transit
 
 #test_get_air()
 actual = transit.get_air(aoi_gdf)
-actual.drop(columns=['OBJECTID'], inplace=True)
+actual.drop(columns=["OBJECTID", "EFF_DATE"], inplace=True)
 # Drop geometry redundant columns that cause trouble
 actual.drop(columns=["LAT_DEG", "LONG_DEG", 'LAT_MIN', 'LONG_MIN'],
             inplace=True)
