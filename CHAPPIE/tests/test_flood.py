f2a13c2c2ae36a71f736d1a857c49fd664052fd0
Justin Bousquin <Bousquin.justin@epa.gov>
2025-09-02 15:48:15 -0500

    test_get_image_by_poly_502_error because aptch decorator no longer exists (removed requests from layer_query)

--- CHAPPIE/tests/test_flood.py
+++ CHAPPIE/tests/test_flood.py
@@ -285,7 +285,7 @@ def polygon_gdf():
     return point_gdf
 
 #Test how 502 server error is handled, but patch the computeStatHist endpoint call
-@patch('CHAPPIE.layer_query.requests.get')
+@patch('CHAPPIE.layer_query.utils.post_request')
 def test_get_image_by_poly_502_error(mock_get, polygon_gdf: geopandas.GeoDataFrame):
     mock_resp = MagicMock()
     mock_resp.status_code = 502
