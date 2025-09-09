7cf176c8fd7172f307ed8f4c3ff09dfa3b13a63e
Justin Bousquin <Bousquin.justin@epa.gov>
2025-09-09 09:47:07 -0500

    lint (function tabbed over too far)

--- CHAPPIE/eco_services/nlcd.py
+++ CHAPPIE/eco_services/nlcd.py
@@ -16,97 +16,97 @@ TIMEOUT = 100
 
 
 def get_NLCD(aoi, year, dataset="Land_Cover"):
-        """Download NLCD raster tiles by polygon extent.
+    """Download NLCD raster tiles by polygon extent.
 
-        If not lower 48 only run one state at a time
+    If not lower 48 only run one state at a time
 
-        Parameters
-        ----------
-        aoi : geopandas.GeoDataframe
-            Area of interest geometries.
-        year : integer or string
-            Year being retrieved.
-        dataset : string
-            Valid datasets include Land_Cover, Tree_Canopy, Impervious, or
-            roads. The default is "Land_Cover".
+    Parameters
+    ----------
+    aoi : geopandas.GeoDataframe
+        Area of interest geometries.
+    year : integer or string
+        Year being retrieved.
+    dataset : string
+        Valid datasets include Land_Cover, Tree_Canopy, Impervious, or
+        roads. The default is "Land_Cover".
 
-        Returns
-        -------
-        geopandas.GeoDataFrame or array
-            Depends on dataset type, array from rasterio
-        """
-        # Make sure dataset parameter is usable
-        datasets = ["Land_Cover", "Impervious", "Tree_Canopy"]
-        assert dataset in datasets, f"'{dataset}' not in NLCD datasets"
-        # Make sure year parameter is usable
-        year = check_year_NLCD(year, dataset)
+    Returns
+    -------
+    geopandas.GeoDataFrame or array
+        Depends on dataset type, array from rasterio
+    """
+    # Make sure dataset parameter is usable
+    datasets = ["Land_Cover", "Impervious", "Tree_Canopy"]
+    assert dataset in datasets, f"'{dataset}' not in NLCD datasets"
+    # Make sure year parameter is usable
+    year = check_year_NLCD(year, dataset)
 
-        bbox = aoi.total_bounds
+    bbox = aoi.total_bounds
 
-        # Create subset X and Y string from extent (minx, miny, maxx, maxy)
-        subset = [
-            'X("{}","{}")'.format(bbox[0], bbox[2]),
-            'Y("{}","{}")'.format(bbox[1], bbox[3]),
-        ]
-        query_crs = aoi.crs.to_epsg()  # CRS for query
+    # Create subset X and Y string from extent (minx, miny, maxx, maxy)
+    subset = [
+        f'X("{bbox[0]}","{bbox[2]}")',
+        f'Y("{bbox[1]}","{bbox[3]}")',
+    ]
+    query_crs = aoi.crs.to_epsg()  # CRS for query
 
-        # Determine landmass (based on FIPs state)
-        st_df = layer_query.get_state_by_aoi(aoi)
-        if not set(st_df.STUSAB).isdisjoint(["AK", "HI", "PR"]):
-            # TODO: not robust for multi: e.g., non-conus, or conus + non-conus
-            landmass = set(st_df.STUSAB).intersection(["AK", "HI", "PR"]).pop()
-        else:
-            landmass = "L48"
-        # Determine serviceName
-        if dataset == "Tree_Canopy" and landmass == "L48":
-            serviceName = f"nlcd_tcc_conus_{year}_v2021-4"
-            coverage = "mrlc_download__" + serviceName
-        else:
-            serviceName = f"NLCD_{year}_{dataset}_{landmass}"
-            coverage = serviceName
+    # Determine landmass (based on FIPs state)
+    st_df = layer_query.get_state_by_aoi(aoi)
+    if not set(st_df.STUSAB).isdisjoint(["AK", "HI", "PR"]):
+        # TODO: not robust for multi: e.g., non-conus, or conus + non-conus
+        landmass = set(st_df.STUSAB).intersection(["AK", "HI", "PR"]).pop()
+    else:
+        landmass = "L48"
+    # Determine serviceName
+    if dataset == "Tree_Canopy" and landmass == "L48":
+        serviceName = f"nlcd_tcc_conus_{year}_v2021-4"
+        coverage = "mrlc_download__" + serviceName
+    else:
+        serviceName = f"NLCD_{year}_{dataset}_{landmass}"
+        coverage = serviceName
 
-        # Source url (to use mrlc_display change outCRS to 3857)
-        # url = f"https://www.mrlc.gov/geoserver/mrlc_display/{coverage}/ows"
-        url = f"https://www.mrlc.gov/geoserver/mrlc_download/{serviceName}/ows"
-        epsg_url = "http://www.opengis.net/def/crs/EPSG/0/"
-        out_crs = 5070  # Service native(mrlc_display/3857, mrlc_download/5070)
+    # Source url (to use mrlc_display change outCRS to 3857)
+    # url = f"https://www.mrlc.gov/geoserver/mrlc_display/{coverage}/ows"
+    url = f"https://www.mrlc.gov/geoserver/mrlc_download/{serviceName}/ows"
+    epsg_url = "http://www.opengis.net/def/crs/EPSG/0/"
+    out_crs = 5070  # Service native(mrlc_display/3857, mrlc_download/5070)
 
-        # Create params dict
-        params = {
-            "service": "WCS",
-            "version": "2.0.1",
-            "request": "GetCoverage",
-            "coverageid": coverage,
-            "subset": subset,
-            "SubsettingCRS": f"{epsg_url}{query_crs}",
-            "format": "image/geotiff",
-            "OutputCRS": "{}{}".format(epsg_url, out_crs),
-        }
+    # Create params dict
+    params = {
+        "service": "WCS",
+        "version": "2.0.1",
+        "request": "GetCoverage",
+        "coverageid": coverage,
+        "subset": subset,
+        "SubsettingCRS": f"{epsg_url}{query_crs}",
+        "format": "image/geotiff",
+        "OutputCRS": f"{epsg_url}{out_crs}",
+    }
 
-        # Get response
-        res = get_url(url, params)
-        res.raise_for_status()  # Check response
+    # Get response
+    res = get_url(url, params)
+    res.raise_for_status()  # Check response
 
-        # # Save result to TempDir
-        with TemporaryDirectory as temp_dir:
-            out_file = os.path.join(temp_dir, f"NLCD_{year}_{dataset}.tif")
-            with open(out_file, "wb") as f:
-                f.write(res.content)
-            # Read in raster using rioxarray
-            rds = rioxarray.open_rasterio(out_file)
+    # # Save result to TempDir
+    with TemporaryDirectory as temp_dir:
+        out_file = os.path.join(temp_dir, f"NLCD_{year}_{dataset}.tif")
+        with open(out_file, "wb") as f:
+            f.write(res.content)
+        # Read in raster using rioxarray
+        rds = rioxarray.open_rasterio(out_file)
 
-        if not rds.rio.crs:
-            # TODO: confirm is none and make more robust?
-            rds.rio.set_crs(f"EPSG:{out_crs}")
-        cell = rds.rio.resolution()
-        rds_prj = rds.rio.reproject(f"EPSG:{3857}", resolution=cell)  # Reproject
-        band1 = rds_prj.to_series()  # numpy array
+    if not rds.rio.crs:
+        # TODO: confirm is none and make more robust?
+        rds.rio.set_crs(f"EPSG:{out_crs}")
+    cell = rds.rio.resolution()
+    rds_prj = rds.rio.reproject(f"EPSG:{3857}", resolution=cell)  # Reproject
+    band1 = rds_prj.to_series()  # numpy array
 
-            # # save it to D2 (drop after testing?)
-            # rds_prj.rio.to_raster(f"{self.D2}{os.sep}{os.path.basename(out_file)}")
-            # # set it
-            # self.set_NLCD(out_file, in_memory=band1)
-        return band1
+        # # save it to D2 (drop after testing?)
+        # rds_prj.rio.to_raster(f"{self.D2}{os.sep}{os.path.basename(out_file)}")
+        # # set it
+        # self.set_NLCD(out_file, in_memory=band1)
+    return band1
 
 
 def check_year_NLCD(year, dataset):
@@ -137,8 +137,8 @@ def check_year_NLCD(year, dataset):
         years = [1992, 1999, 2002, 2003] + list(range(2008, latest))
     else:
         years = "Problem"
-    assert year < latest, "Only available through {}".format(max(years))
-    assert year in years, "Year {} is not in {}".format(year, years)
+    assert year < latest, f"Only available through {max(years)}"
+    assert year in years, f"Year {year} is not in {years}"
     return year
 
 
@@ -164,5 +164,5 @@ def get_url(url, data=None):
     else:
         res = requests.get(url, timeout=TIMEOUT)
     # TODO: watch out for res.url vs url (added back, possibly removed it once)
-    assert res.ok, "Problem with {}".format(res.url)
+    assert res.ok, f"Problem with {res.url}"
     return res
